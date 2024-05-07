b695cb3dafe42bb8ccd4752bbcac726658ea0bbb
Priyank Pahwa <144533970+Priyank-0401@users.noreply.github.com>
2024-05-07 19:20:02 +0530

    Add files via upload

---
+++ core/db/database.php
@@ -0,0 +1,9 @@
+<?php
+    $host= 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'user';
+
+
+    $con = mysqli_connect("localhost","root","","user") or die(mysqli_connect_error());
+?>
